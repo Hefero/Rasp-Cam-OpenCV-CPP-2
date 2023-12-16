fd0eac2e21f03409778282bc6e0199eee681f0bf
Hefero <guilherme.marques00@hotmail.com>
2023-12-16 13:32:41 -0300

    t

--- aula9/MotorH/MotorH.cpp
+++ aula9/MotorH/MotorH.cpp
@@ -10,8 +10,8 @@
 #include "MotorH.h"
 
 
-#define velDir 80
-#define velEsq 100
+#define velDir 70
+#define velEsq 70
 #define LEFT_TURN_DISTANCE 1
 #define RIGHT_TURN_DISTANCE 1
 
@@ -57,7 +57,7 @@ void motorH::hardStop(void)
 void motorH::turnLeft(void)
 {
     moving = true;
-    motorDir(1, velDir/2);
+    motorDir(1, 60);
     motorEsq(0, velEsq);
 }
 
@@ -65,7 +65,7 @@ void motorH::turnRight(void)
 {
     moving = true;
     motorDir(0, velDir);
-    motorEsq(1, velEsq/2);
+    motorEsq(1, 60);
 }   
 
 void motorH::motorDir(int command, int duty)
